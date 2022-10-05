5422353231c11957dc8857b83f2028d85d92d629
saugat10 <saugatkhatiwada10@gmail.com>
2022-10-06 00:50:56 +0200

    Add project files.

---
+++ Football/FootballPlayer.cs
@@ -0,0 +1,35 @@
+﻿namespace Football
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public override string ToString()
+        {
+            return "The player with id " + Id + " has name " + Name + " age is " + Age + " and wears a shirt number "+ ShirtNumber;
+        }
+
+        public void ValidateName()
+        {
+            if (Name == null)
+                throw new ArgumentNullException(nameof(Name));
+
+            if (Name.Length < 2)
+                throw new ArgumentException(nameof(Name));
+        }
+
+        public void ValidateAge()
+        {
+            if (Age < 1)
+                throw new ArgumentOutOfRangeException(nameof(Age));
+        }
+        public void ValidateShirtNumber()
+        {
+            if (ShirtNumber <= 1 || ShirtNumber >= 99)
+                throw new ArgumentOutOfRangeException(nameof(Age));
+        }
+    }
+}
---
+++ FootballTests/FootballPlayerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Football;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        [TestMethod()]
+        public void ValidateName()
+        {
+            var player = new FootballPlayer() { Id = 1, Name = null, Age = 5, ShirtNumber = 18 };
+            Assert.ThrowsException<ArgumentNullException>(() => player.ValidateName());
+
+            var playerName = new FootballPlayer() { Id = 1, Name = "s", Age = 5, ShirtNumber = 18 };
+            Assert.ThrowsException<ArgumentException>(() => playerName.ValidateName());
+        }
+
+        [TestMethod()]
+        public void ValidateAgeTest()
+        {
+            var playerAge = new FootballPlayer() { Id = 1, Name = "Sabin Bhandari", Age = 0, ShirtNumber = 23 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => playerAge.ValidateAge());
+        }
+
+        [TestMethod()]
+        public void ValidateShirtNumberTest()
+        {
+            var playerShirtNumber = new FootballPlayer() { Id = 1, Name = "Sabin Ghimire", Age = 22, ShirtNumber = 0 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => playerShirtNumber.ValidateShirtNumber());
+
+            var playerShirt = new FootballPlayer() { Id = 2, Name = "Saugat Khatiwada", Age = 25, ShirtNumber = 100 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => playerShirt.ValidateShirtNumber());
+        }
+
+        [TestMethod()]
+        public void ValidateToStringMethod()
+        {
+           
+            var playerShirtNumber = new FootballPlayer() { Id = 1, Name = "Sabin Ghimire", Age = 22, ShirtNumber = 22 };
+            Assert.AreEqual("The player with id 1 has name Sabin Ghimire age is 22 and wears a shirt number 22", playerShirtNumber.ToString());
+        }
+    }
+}
